f62503e88425ef1173bc682de168d6e169c2219e
alistairwalsh <alistairwalsh@me.com>
2023-12-14 13:57:39 +1100

    method

---
+++ pages/Method.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.header('Method')
+
+st.markdown('''The data from the correlation matrices for each participant was initially flattened and repeats removed (i.e. the upper triangle), as well as
+             the diagonal that represents the correlation of an area to itself which is always 1. 
+            ''')
