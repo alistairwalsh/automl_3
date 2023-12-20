925710db68429d88b950818880990d60dd3e4ff7
Alistair Walsh <alistair.walsh@me.com>
2023-12-21 09:12:31 +1100

    Intro start

---
+++ pages/Introduction.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.header('Introduction')
+
+st.write('''
+         
+Artificial intelligence (AI) is a broader concept that refers to the development of computer systems capable of performing tasks that typically require human intelligence. Machine learning (ML) is a subset of AI, 
+         focused on training algorithms to 
+         learn patterns from data and make predictions or decisions without explicit programming. In essence, machine learning is a key technology within the field of artificial intelligence.
+         ''')
