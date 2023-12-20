17b01f8590468ae64245aa5148f1bba23ae8b44c
Alistair Walsh <alistair.walsh@me.com>
2023-12-21 09:14:09 +1100

    more intro

--- pages/Introduction.py
+++ pages/Introduction.py
@@ -7,4 +7,13 @@ st.write('''
 Artificial intelligence (AI) is a broader concept that refers to the development of computer systems capable of performing tasks that typically require human intelligence. Machine learning (ML) is a subset of AI, 
          focused on training algorithms to 
          learn patterns from data and make predictions or decisions without explicit programming. In essence, machine learning is a key technology within the field of artificial intelligence.
+         Hyperparameter tuning involves optimizing the configuration settings, known as hyperparameters, of a machine learning model to achieve better performance. These hyperparameters are not learned from the 
+         data but are set prior to the training process. The goal of hyperparameter tuning is to find the combination that results in the most effective and accurate model for a given task or dataset.
+
+         Neural networks typically require larger amounts of data compared to classical machine learning models. This is because neural networks, especially deep learning models, have a high number of 
+         parameters that need to be optimized, and a large dataset helps prevent overfitting and allows the model to generalize well to unseen data.
+
+Classical machine learning models, such as decision trees or linear regression, may perform reasonably well with smaller datasets, as they have fewer parameters and dependencies to learn.
+
+In summary, neural networks often benefit from big datasets to effectively learn complex patterns and relationships, while some classical machine learning models can perform adequately with smaller datasets.
          ''')
